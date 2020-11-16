eaa8144893687e79f26458506da4986ec84bdb40
Vladimir <vzh.inbox@gmail.com>
2020-11-16 17:56:46 +0600

    Add crossOrigin

--- src/loadImage.ts
+++ src/loadImage.ts
@@ -1,7 +1,11 @@
 /** Don't worry, it uses cached by browser image if url already loaded previously otherwise cache the image. */
-export default function loadImage(imageUrl: string): Promise<HTMLImageElement> {
+export default function loadImage(
+  imageUrl: string,
+  crossOrigin?: string
+): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image();
+    img.crossOrigin = crossOrigin ?? null;
     img.onload = () => {
       resolve(img);
     };
