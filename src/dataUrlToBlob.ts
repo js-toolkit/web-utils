e150f152bfccf4c31c85d45973de1255b093cd23
Vladimir <vzh.inbox@gmail.com>
2020-12-17 12:17:01 +0600

    Refactor dataUrlToBlob

--- src/dataUrlToBlob.ts
+++ src/dataUrlToBlob.ts
@@ -1,7 +1,14 @@
+// https://stackoverflow.com/a/12300351
 export default function dataUrlToBlob(dataUrl: string): Blob {
   const [type, base64] = dataUrl.split(',');
   const byteString = window.atob(base64);
   const mimeString = type.split(':')[1].split(';')[0];
-  const ab = Uint8Array.from(byteString, (ch) => ch.charCodeAt(0)).buffer;
+
+  const ab = new ArrayBuffer(byteString.length);
+  const ia = new Uint8Array(ab);
+  for (let i = 0; i < byteString.length; i += 1) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+
   return new Blob([ab], { type: mimeString });
 }
