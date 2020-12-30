77c951abb8cafff4599f281869b3ab6fd09159ba
Vladimir <vzh.inbox@gmail.com>
2020-12-30 11:13:52 +0600

    Add onDOMReady

---
+++ src/onDOMReady.ts
@@ -0,0 +1,11 @@
+export default function onDOMReady(callback: VoidFunction): VoidFunction {
+  if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', callback, { once: true });
+  } else {
+    callback();
+  }
+
+  return (): void => {
+    document.removeEventListener('DOMContentLoaded', callback);
+  };
+}
