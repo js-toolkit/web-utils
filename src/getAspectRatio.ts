64a4231a526cecc3309ae7334d1e711dac34e254
Vladimir <vzh.inbox@gmail.com>
2021-01-21 11:39:34 +0600

    Add ratio

--- src/getAspectRatio.ts
+++ src/getAspectRatio.ts
@@ -4,9 +4,13 @@ import toInt from '@vzh/ts-utils/toInt';
 export interface AspectRatio {
   width: number;
   height: number;
+  ratio: number;
 }
 
 export default function getAspectRatio(width: number, height: number): AspectRatio {
   const gcdValue = gcd(width, height);
-  return { width: toInt(width / gcdValue), height: toInt(height / gcdValue) };
+  const widthRatio = toInt(width / gcdValue);
+  const heightRatio = toInt(height / gcdValue);
+  const ratio = width / height;
+  return { width: widthRatio, height: heightRatio, ratio };
 }
