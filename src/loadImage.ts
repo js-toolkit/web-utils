3a9bf1325e31fab21e0dc4f60041127445b08bdd
Vladimir <vzh.inbox@gmail.com>
2021-03-04 10:04:08 +0600

    Improve loadImage

--- src/loadImage.ts
+++ src/loadImage.ts
@@ -1,11 +1,13 @@
 /** Don't worry, it uses cached by browser image if url already loaded previously otherwise cache the image. */
 export default function loadImage(
   imageUrl: string,
-  crossOrigin?: string
+  crossOrigin?: HTMLImageElement['crossOrigin']
 ): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image();
-    img.crossOrigin = crossOrigin ?? null;
+    if (crossOrigin !== undefined) {
+      img.crossOrigin = crossOrigin;
+    }
     img.onload = () => {
       resolve(img);
     };
