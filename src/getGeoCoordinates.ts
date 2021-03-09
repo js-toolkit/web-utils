12eec058cd6e4112de461d78a06f51b5038ffd8a
Vladimir <vzh.inbox@gmail.com>
2021-03-09 15:59:34 +0600

    Add getGeoCoordinates

---
+++ src/getGeoCoordinates.ts
@@ -0,0 +1,13 @@
+export default function getGeoCoordinates({
+  timeout = 10000,
+  maximumAge = 60000,
+  ...rest
+}: PositionOptions = {}): Promise<GeolocationPosition> {
+  return new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(
+      (pos) => resolve(pos),
+      (err) => reject(err),
+      { timeout, maximumAge, ...rest }
+    )
+  );
+}
