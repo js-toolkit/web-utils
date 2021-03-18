1f16b8f2e06fd82152893244a3584cd1a53959b6
Vladimir <vzh.inbox@gmail.com>
2021-03-18 12:08:26 +0600

    Add getBrowserLanguage

---
+++ src/getBrowserLanguage.ts
@@ -0,0 +1,16 @@
+declare global {
+  interface Navigator {
+    userLanguage: string;
+  }
+}
+
+export default function getBrowserLanguage(): string | undefined {
+  if (typeof window !== 'undefined' && 'navigator' in window) {
+    const language =
+      navigator.languages && navigator.languages.length > 0
+        ? navigator.languages[0]
+        : navigator.language || navigator.userLanguage;
+    return language.split('-')[0];
+  }
+  return undefined;
+}
