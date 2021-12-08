7e13175236adc58f9a2e1cb2b882e5adb1e12e5d
Vladimir <vzh.inbox@gmail.com>
2021-12-08 21:34:39 +0600

    Add custom prop to texttrack

--- src/toggleNativeSubtitles.ts
+++ src/toggleNativeSubtitles.ts
@@ -1,10 +1,18 @@
+declare global {
+  interface TextTrack {
+    ignoreChange?: boolean;
+  }
+}
+
 export default function toggleNativeSubtitles(native: boolean, textTracks: TextTrackList): void {
   // console.log('toggleNativeSubtitles');
   for (let i = 0; i < textTracks.length; i += 1) {
     const track = textTracks[i];
     if (native && track.mode === 'hidden') {
+      track.ignoreChange = true;
       track.mode = 'showing';
     } else if (!native && track.mode === 'showing') {
+      delete track.ignoreChange;
       track.mode = 'hidden';
     }
     // console.log(track.label, track.mode);
