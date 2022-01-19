c5820f5db022b786144dca7f7bfb60ddd2a487f6
Vladimir <vzh.inbox@gmail.com>
2022-01-19 16:27:12 +0600

    Add pseudo fullscreen and improve

--- src/FullscreenController.ts
+++ src/FullscreenController.ts
@@ -1,5 +1,5 @@
-import EventEmitter from 'eventemitter3';
-import fullscreen, { getFullscreenUnavailableError } from './fullscreen';
+import EventEmitter, { EventListener } from 'eventemitter3';
+import fullscreen from './fullscreen';
 import toggleNativeSubtitles from './toggleNativeSubtitles';
 
 declare global {
@@ -16,18 +16,25 @@ export enum FullscreenControllerEvent {
   Error = 'error',
 }
 
-export type FullscreenControllerEventMap = {
-  [FullscreenControllerEvent.Change]: [{ isFullscreen: boolean; video: boolean }];
-  [FullscreenControllerEvent.Error]: [{ error: unknown; video: boolean }];
+type FullscreenControllerEventMap = {
+  [FullscreenControllerEvent.Change]: [
+    { isFullscreen: boolean; video?: boolean; pseudo?: boolean }
+  ];
+  [FullscreenControllerEvent.Error]: [{ error: unknown; video?: boolean; pseudo?: boolean }];
 };
 
 export interface FullscreenRequestOptions extends Readonly<FullscreenOptions> {
   /** Used for iOS */
   readonly toggleNativeVideoSubtitles?: boolean;
+  readonly pseudoFullscreenFallback?: boolean;
 }
 
-export default class FullscreenController extends EventEmitter<FullscreenControllerEventMap> {
+class FullscreenController extends EventEmitter<FullscreenControllerEventMap> {
   readonly Events = FullscreenControllerEvent;
+
+  private pseudo:
+    | Pick<CSSStyleDeclaration, 'position' | 'left' | 'top' | 'width' | 'height' | 'zIndex'>
+    | undefined = undefined;
 
   constructor(private readonly element: Element, private readonly video?: HTMLVideoElement) {
     super();
@@ -57,37 +64,34 @@ export default class FullscreenController extends EventEmitter<FullscreenControl
 
   // eslint-disable-next-line class-methods-use-this
   get isFullscreenSupported(): boolean {
-    return fullscreen.isEnabled;
+    return fullscreen.isEnabled();
   }
 
   get isFullscreen(): boolean {
-    return fullscreen.names
-      ? document[fullscreen.names.fullscreenElementName] === this.element
-      : !!this.video?.webkitDisplayingFullscreen;
+    return !!this.currentElement;
+  }
+
+  get isPseudoFullscreen(): boolean {
+    return this.isFullscreen && !!this.pseudo;
   }
 
   get currentElement(): Element | null {
-    if (fullscreen.names) {
-      if (document[fullscreen.names.fullscreenElementName] === this.element) return this.element;
+    if (fullscreen.isSupported()) {
+      if (fullscreen.getElement() === this.element) return this.element;
     } else if (this.video?.webkitDisplayingFullscreen) {
       return this.video;
+    } else if (this.pseudo) {
+      return this.element;
     }
     return null;
   }
 
   private changeHandler = (): void => {
-    const { names } = fullscreen;
-    if (!names) return;
-    this.emit(FullscreenControllerEvent.Change, {
-      isFullscreen: document[names.fullscreenElementName] === this.element,
-      video: false,
-    });
+    this.emit(FullscreenControllerEvent.Change, { isFullscreen: this.isFullscreen });
   };
 
   private errorHandler = (event: Event): void => {
-    const { names } = fullscreen;
-    if (!names) return;
-    this.emit(FullscreenControllerEvent.Error, { error: event, video: false });
+    this.emit(FullscreenControllerEvent.Error, { error: event });
   };
 
   private beginFullscreenHandler = (): void => {
@@ -105,42 +109,70 @@ export default class FullscreenController extends EventEmitter<FullscreenControl
         return;
       }
 
-      const { toggleNativeVideoSubtitles, ...rest } = options;
+      const { toggleNativeVideoSubtitles, pseudoFullscreenFallback, ...rest } = options;
 
-      if (fullscreen.isEnabled) {
+      if (fullscreen.isEnabled()) {
         fullscreen.request(this.element, rest).then(resolve, reject);
         return;
       }
 
       const { video } = this;
+      if (video?.webkitEnterFullscreen) {
+        if (video.webkitDisplayingFullscreen) {
+          resolve();
+          return;
+        }
 
-      if (!video?.webkitEnterFullscreen) {
-        reject(getFullscreenUnavailableError());
-        return;
-      }
+        if (toggleNativeVideoSubtitles && video.textTracks.length > 0) {
+          toggleNativeSubtitles(true, video.textTracks);
 
-      if (video.webkitDisplayingFullscreen) {
-        resolve();
-        return;
-      }
+          const endFullscreenHandler = (): void => {
+            video.removeEventListener('webkitendfullscreen', endFullscreenHandler);
+            toggleNativeSubtitles(false, video.textTracks);
+          };
+          video.addEventListener('webkitendfullscreen', endFullscreenHandler);
+        }
 
-      if (toggleNativeVideoSubtitles && video.textTracks.length > 0) {
-        toggleNativeSubtitles(true, video.textTracks);
-
-        const endFullscreenHandler = (): void => {
-          video.removeEventListener('webkitendfullscreen', endFullscreenHandler);
-          toggleNativeSubtitles(false, video.textTracks);
+        const beginFullscreenHandler = (): void => {
+          video.removeEventListener('webkitbeginfullscreen', beginFullscreenHandler);
+          resolve();
         };
-        video.addEventListener('webkitendfullscreen', endFullscreenHandler);
+
+        video.addEventListener('webkitbeginfullscreen', beginFullscreenHandler);
+        video.webkitEnterFullscreen();
+
+        return;
       }
 
-      const beginFullscreenHandler = (): void => {
-        video.removeEventListener('webkitbeginfullscreen', beginFullscreenHandler);
-        resolve();
-      };
+      if (pseudoFullscreenFallback) {
+        if (this.pseudo) {
+          resolve();
+          return;
+        }
 
-      video.addEventListener('webkitbeginfullscreen', beginFullscreenHandler);
-      video.webkitEnterFullscreen();
+        const el = this.element as HTMLElement;
+        this.pseudo = {
+          position: el.style.position,
+          left: el.style.left,
+          top: el.style.top,
+          width: el.style.width,
+          height: el.style.height,
+          zIndex: el.style.zIndex,
+        };
+        el.style.position = 'fixed';
+        el.style.left = '0px';
+        el.style.top = '0px';
+        el.style.width = '100vw';
+        el.style.height = '100vh';
+        el.style.zIndex = '99999';
+
+        this.emit(FullscreenControllerEvent.Change, { isFullscreen: true, pseudo: true });
+
+        resolve();
+        return;
+      }
+
+      reject(new fullscreen.UnavailableError());
     });
   }
 
@@ -151,30 +183,58 @@ export default class FullscreenController extends EventEmitter<FullscreenControl
         return;
       }
 
-      if (fullscreen.isEnabled) {
+      if (fullscreen.isEnabled()) {
         fullscreen.exit().then(resolve, reject);
         return;
       }
 
       const { video } = this;
+      if (video?.webkitExitFullscreen) {
+        if (!video.webkitDisplayingFullscreen) {
+          resolve();
+          return;
+        }
 
-      if (!video?.webkitExitFullscreen) {
-        reject(getFullscreenUnavailableError());
+        const endFullscreenHandler = (): void => {
+          video.removeEventListener('webkitendfullscreen', endFullscreenHandler);
+          resolve();
+        };
+
+        video.addEventListener('webkitendfullscreen', endFullscreenHandler);
+        video.webkitExitFullscreen();
+
         return;
       }
 
-      if (!video.webkitDisplayingFullscreen) {
+      if (this.pseudo) {
+        const el = this.element as HTMLElement;
+        el.style.position = this.pseudo.position;
+        el.style.left = this.pseudo.left;
+        el.style.top = this.pseudo.top;
+        el.style.width = this.pseudo.width;
+        el.style.height = this.pseudo.height;
+        el.style.zIndex = this.pseudo.zIndex;
+        this.pseudo = undefined;
+
+        this.emit(FullscreenControllerEvent.Change, { isFullscreen: false, pseudo: true });
+
         resolve();
         return;
       }
 
-      const endFullscreenHandler = (): void => {
-        video.removeEventListener('webkitendfullscreen', endFullscreenHandler);
-        resolve();
-      };
-
-      video.addEventListener('webkitendfullscreen', endFullscreenHandler);
-      video.webkitExitFullscreen();
+      reject(new fullscreen.UnavailableError());
     });
   }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+namespace FullscreenController {
+  export type EventMap = FullscreenControllerEventMap;
+  export type EventHandler<T extends FullscreenControllerEvent = FullscreenControllerEvent> =
+    EventListener<EventMap, T>;
+  export type EventHandlerMap<T extends FullscreenControllerEvent = FullscreenControllerEvent> = {
+    [P in T]: EventHandler<P>;
+  };
+}
+
+export default FullscreenController;
