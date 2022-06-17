533d95f608783f13078e2273ec384c7efeed24e9
Vladimir <vzh.inbox@gmail.com>
2022-06-17 11:48:25 +0600

    Improve isIOS

--- src/detection/isIOS.ts
+++ src/detection/isIOS.ts
@@ -2,7 +2,7 @@ let memo: boolean;
 
 export default function isIOS(): boolean {
   if (memo == null) {
-    memo = /(iPhone|iPad|iPod)/i.test(navigator.userAgent);
+    memo = /[(\s](iPhone|iPad|iPod)/i.test(navigator.userAgent);
   }
   return memo;
 }
