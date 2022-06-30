19b30ec7191110fea52c2105bc90c81d5489509c
Vladimir <vzh.inbox@gmail.com>
2022-06-30 10:53:13 +0600

    Improve copyToClipboard

--- src/copyToClipboard.ts
+++ src/copyToClipboard.ts
@@ -1,31 +1,35 @@
-export default function copyToClipboard(text: string): boolean {
-  const textarea = document.createElement('textarea');
-  textarea.textContent = text;
-  textarea.style.position = 'fixed';
-  textarea.style.bottom = '-100px';
-  textarea.readOnly = true; // To avoid showing keyboard;
-  document.body.appendChild(textarea);
-
-  const range = document.createRange();
-  range.selectNode(textarea);
-  const selection = window.getSelection();
-  if (!selection) {
-    console.warn('No selection for copy to clipboard.');
-    return false;
+export function copyToClipboard(text: string): Promise<void> {
+  if (navigator.clipboard) {
+    return navigator.clipboard.writeText(text);
   }
-  selection.removeAllRanges();
-  selection.addRange(range);
-  textarea.setSelectionRange(0, 999999);
 
-  try {
-    document.execCommand('copy', false);
-    return true;
-  } catch (err) {
-    console.error(err);
-  } finally {
+  return new Promise((resolve, reject) => {
+    const textarea = document.createElement('textarea');
+    textarea.textContent = text;
+    textarea.style.position = 'fixed';
+    textarea.style.bottom = '-100px';
+    textarea.readOnly = true; // To avoid showing keyboard;
+    document.body.appendChild(textarea);
+
+    const range = document.createRange();
+    range.selectNode(textarea);
+    const selection = window.getSelection();
+    if (!selection) {
+      reject(new Error('No selection for copy to clipboard.'));
+      return;
+    }
     selection.removeAllRanges();
-    document.body.removeChild(textarea);
-  }
+    selection.addRange(range);
+    textarea.setSelectionRange(0, 999999);
 
-  return false;
+    try {
+      document.execCommand('copy', false);
+      resolve();
+    } catch (err) {
+      reject(err);
+    } finally {
+      selection.removeAllRanges();
+      document.body.removeChild(textarea);
+    }
+  });
 }
