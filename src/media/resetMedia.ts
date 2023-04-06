7ef802c400e1f9f5d43ea6e23c2ff7ab75e0b80f
Vladimir <vzh.inbox@gmail.com>
2023-04-06 10:32:16 +0600

    Improve resetMedia

--- src/media/resetMedia.ts
+++ src/media/resetMedia.ts
@@ -3,6 +3,7 @@
 /** Remove media from hardware decoder buffer */
 export function resetMedia(media: HTMLMediaElement): void {
   if (media.src) {
+    URL.revokeObjectURL(media.src);
     media.src = '';
     media.srcObject = null;
     media.load();
