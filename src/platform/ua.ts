292c2633e54dc286aa5fcf58d32f134a4e22a92f
Vladimir <vzh.inbox@gmail.com>
2023-12-20 10:03:27 +0600

    Improve ua

--- src/platform/ua.ts
+++ src/platform/ua.ts
@@ -1,14 +1,17 @@
 import { UAParser } from 'ua-parser-js';
 
-type UAInfo = DeepReadonly<OmitStrict<UAParser.IResult, 'withClientHints'>>;
+type UAInfo = DeepReadonly<OmitStrict<UAParser.IResult, 'withClientHints' | 'withFeatureCheck'>>;
 
 let result: UAInfo | undefined;
-let promise: Promise<UAInfo> | undefined;
+let promise: Promise<UAParser.IResult> | undefined;
 
 export async function getUAInfo(): Promise<UAInfo> {
   if (result == null) {
     promise =
-      promise ?? Promise.resolve(new UAParser(navigator.userAgent).getResult().withClientHints());
+      promise ??
+      Promise.resolve(new UAParser(navigator.userAgent).getResult().withClientHints()).then((res) =>
+        res.withFeatureCheck()
+      );
     result = await promise;
     promise = undefined;
   }
@@ -16,5 +19,9 @@ export async function getUAInfo(): Promise<UAInfo> {
 }
 
 export function getCachedUAInfo(): UAInfo | undefined {
+  if (result == null) {
+    console.warn('UAInfo is not ready yet.');
+    void getUAInfo();
+  }
   return result;
 }
