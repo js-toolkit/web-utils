dc869192cfb64ac37110661bbd9de9f75c269b91
Vladimir <vzh.inbox@gmail.com>
2024-01-23 13:11:40 +0600

    Add ManagedMediaSource type

--- src/media/mse/getMediaSource.ts
+++ src/media/mse/getMediaSource.ts
@@ -1,6 +1,28 @@
 declare global {
+  interface ManagedMediaSourceEventMap extends MediaSourceEventMap {
+    startstreaming: Event;
+    endstreaming: Event;
+  }
+
+  interface ManagedMediaSource extends MediaSource {
+    readonly streaming: boolean;
+    onstartstreaming: ((this: ManagedMediaSource, ev: Event) => any) | null;
+    onendstreaming: ((this: ManagedMediaSource, ev: Event) => any) | null;
+    addEventListener<K extends keyof ManagedMediaSourceEventMap>(
+      type: K,
+      listener: (this: MediaSource, ev: ManagedMediaSourceEventMap[K]) => any,
+      options?: boolean | AddEventListenerOptions
+    ): void;
+    removeEventListener<K extends keyof ManagedMediaSourceEventMap>(
+      type: K,
+      listener: (this: MediaSource, ev: ManagedMediaSourceEventMap[K]) => any,
+      options?: boolean | EventListenerOptions
+    ): void;
+  }
+
   interface Window {
     WebKitMediaSource?: typeof MediaSource;
+    ManagedMediaSource?: ManagedMediaSource;
   }
 }
 
