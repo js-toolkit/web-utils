1d27008f0d133980f2269f56a79526df1fd60aab
Vladimir <vzh.inbox@gmail.com>
2024-01-26 11:32:54 +0600

    Add Capabilities

---
+++ src/media/Capabilities.ts
@@ -0,0 +1,47 @@
+import { getMediaSource } from './getMediaSource';
+
+export abstract class Capabilities {
+  private static readonly supportMap = new Map<string, boolean>();
+  private static readonly canPlayMap = new Map<string, boolean>();
+  private static tmpVideo: HTMLVideoElement | undefined;
+  private static cacheTimer: number | undefined;
+
+  /** Check video element. Mime type and optional codecs. */
+  static isCanPlayType(type: string): boolean {
+    if (this.canPlayMap.has(type)) {
+      return !!this.canPlayMap.get(type);
+    }
+    if (!this.tmpVideo) {
+      this.tmpVideo = document.getElementsByTagName('video')[0] || document.createElement('video');
+      // Release handle for GC work.
+      window.clearTimeout(this.cacheTimer);
+      this.cacheTimer = window.setTimeout(() => {
+        this.tmpVideo = undefined;
+      }, 1000);
+    }
+    const support = !!this.tmpVideo.canPlayType(type);
+    this.supportMap.set(type, support);
+    return support;
+  }
+
+  /** Check MediaSource. */
+  static isTypeSupported(type: string): boolean {
+    if (this.supportMap.has(type)) {
+      return !!this.supportMap.get(type);
+    }
+    const mediaSource = getMediaSource();
+    if (mediaSource) {
+      const support = mediaSource.isTypeSupported(type);
+      this.supportMap.set(type, support);
+      return support;
+    }
+    return false;
+  }
+
+  static reset(): void {
+    window.clearTimeout(this.cacheTimer);
+    this.tmpVideo = undefined;
+    this.supportMap.clear();
+    this.canPlayMap.clear();
+  }
+}
