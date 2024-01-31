50a0f583bc57e5d4914a5e66e1c9aadb8bf1ec7d
Vladimir <vzh.inbox@gmail.com>
2024-01-31 17:39:13 +0600

    Improve isMSESupported

--- src/platform/isMSESupported.ts
+++ src/platform/isMSESupported.ts
@@ -1,7 +1,26 @@
 import { getMediaSource } from '../media/getMediaSource';
 
+declare global {
+  interface Window {
+    WebKitSourceBuffer?: typeof SourceBuffer;
+  }
+}
+
+function getSourceBuffer(): typeof SourceBuffer | undefined {
+  return window.SourceBuffer || window.WebKitSourceBuffer;
+}
+
 /** Media Source Extensions */
 export function isMSESupported(): boolean {
   // Some very old MediaSource implementations didn't have isTypeSupported.
-  return !!getMediaSource()?.isTypeSupported;
+  if (!getMediaSource()?.isTypeSupported) return false;
+  // if SourceBuffer is exposed ensure its API is valid.
+  // Older browsers do not expose SourceBuffer globally so checking SourceBuffer.prototype is impossible.
+  const sourceBuffer = getSourceBuffer();
+  return (
+    !sourceBuffer ||
+    (sourceBuffer.prototype &&
+      typeof sourceBuffer.prototype.appendBuffer === 'function' &&
+      typeof sourceBuffer.prototype.remove === 'function')
+  );
 }
