b896b0275cc78b64a776a222dd09b94b14e54ad8
Vladimir <vzh.inbox@gmail.com>
2024-02-06 17:53:02 +0600

    Add MediaNotAttachedError

---
+++ src/media/MediaNotAttachedError.ts
@@ -0,0 +1,10 @@
+import { ErrorCompat } from '@js-toolkit/utils/ErrorCompat';
+
+export class MediaNotAttachedError extends ErrorCompat {
+  constructor(
+    message = 'Media element is not attached yet.',
+    options: ErrorOptions | undefined = undefined
+  ) {
+    super(MediaNotAttachedError, message, options);
+  }
+}
