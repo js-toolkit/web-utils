225d7636139e55e6bc0e5af4e6f1736594c4026f
Vladimir <vzh.inbox@gmail.com>
2024-02-29 11:40:19 +0600

    Improve onPageReady

--- src/onPageReady.ts
+++ src/onPageReady.ts
@@ -1,16 +1,35 @@
+import { delay } from '@js-toolkit/utils/delay';
+
 export function isPageReady(): boolean {
   return document.readyState === 'complete';
 }
 
-/** @returns cancel wait function */
-export function onPageReady(callback: VoidFunction): VoidFunction {
+interface Options {
+  readonly timeout?: number | undefined;
+}
+
+/** @returns cancel wait function. */
+export function onPageReady(callback: VoidFunction, options?: Options): VoidFunction {
+  let delayed: delay.Delay | undefined;
+
+  const cancel = (): void => {
+    window.removeEventListener('load', cbWrapper);
+    delayed?.cancel();
+  };
+
+  const cbWrapper = (): void => {
+    cancel();
+    callback();
+  };
+
   if (isPageReady()) {
     callback();
   } else {
-    window.addEventListener('load', callback, { once: true });
+    window.addEventListener('load', cbWrapper, { once: true });
+    if (options?.timeout) {
+      delayed = delay(cbWrapper, options.timeout);
+    }
   }
 
-  return (): void => {
-    window.removeEventListener('load', callback);
-  };
+  return cancel;
 }
