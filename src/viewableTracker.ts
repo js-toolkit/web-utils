35238e5ecf54cd0a43403b7cc6a2a5360fff40eb
Vladimir <vzh.inbox@gmail.com>
2024-05-31 17:59:23 +0500

    Add ViewableTracker

---
+++ src/viewableTracker.ts
@@ -0,0 +1,98 @@
+import throttleFn from 'lodash.throttle';
+
+export interface ViewableTrackerOptions {
+  /** Visibility part to detect state as viewable. */
+  readonly visiblePart?: boolean | number | undefined;
+  readonly scrollThrottle?: number | undefined;
+  readonly documentVisibility?: boolean | undefined;
+  readonly onChange: (viewable: boolean) => void;
+}
+
+export interface ViewableTracker {
+  readonly check: VoidFunction;
+  readonly destroy: VoidFunction;
+}
+
+export function getViewableTracker(
+  element: HTMLElement,
+  {
+    visiblePart: visiblePartOption = 0.8,
+    scrollThrottle = 200,
+    documentVisibility = true,
+    onChange,
+  }: ViewableTrackerOptions
+): ViewableTracker {
+  const visiblePart = +visiblePartOption;
+  let raf = 0;
+  let lastViewable: boolean | undefined;
+  let lastDocumentViewable = document.visibilityState === 'visible';
+
+  const checkVisibility = (): void => {
+    if (documentVisibility && document.visibilityState !== 'visible') return;
+    const { top, bottom, height } = element.getBoundingClientRect();
+    const visibleHeight = height * visiblePart;
+    const bottomPos = top + visibleHeight;
+    const viewable = window.innerHeight >= bottomPos && bottom > visibleHeight;
+    // console.log(bottomPos, top, bottom, visibleHeight, viewable);
+    if (lastViewable !== viewable) {
+      lastViewable = viewable;
+      onChange(viewable);
+    }
+  };
+
+  const checkVisibilityRaf = (): void => {
+    if (documentVisibility && document.visibilityState !== 'visible') return;
+    raf = window.requestAnimationFrame(checkVisibility);
+  };
+
+  const checkDocumentVisibility = (): void => {
+    const viewable = document.visibilityState === 'visible';
+    if (lastDocumentViewable === viewable) return;
+    lastDocumentViewable = viewable;
+    // Check if visible on page
+    if (viewable && visiblePart > 0) {
+      // Always re-check, the page size may be changed or something else.
+      lastViewable = undefined;
+      checkVisibility();
+    }
+    // Hidden tab
+    else if (visiblePart <= 0 || lastViewable !== viewable) {
+      onChange(viewable);
+    }
+  };
+
+  const handler =
+    scrollThrottle && scrollThrottle > 0
+      ? throttleFn(checkVisibility, scrollThrottle)
+      : checkVisibilityRaf;
+
+  if (visiblePart > 0) {
+    window.addEventListener('scroll', handler, { capture: false, passive: true });
+    // checkVisibility();
+  }
+
+  if (documentVisibility) {
+    document.addEventListener('visibilitychange', checkDocumentVisibility);
+    // checkDocumentVisibility();
+  }
+
+  const check = (): void => {
+    if (visiblePart > 0) {
+      checkVisibility();
+    }
+    if (documentVisibility) {
+      checkDocumentVisibility();
+    }
+  };
+
+  const destroy = (): void => {
+    cancelAnimationFrame(raf);
+    window.removeEventListener('scroll', handler, { capture: false });
+    document.removeEventListener('visibilitychange', checkDocumentVisibility);
+  };
+
+  return {
+    check,
+    destroy,
+  };
+}
