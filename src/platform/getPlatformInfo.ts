b7fd4183d1a9a1665ecfdf0460bf0676b77d98f4
Vladimir <vzh.inbox@gmail.com>
2024-07-10 12:34:07 +0500

    Add getPlatformInfoSync

--- src/platform/getPlatformInfo.ts
+++ src/platform/getPlatformInfo.ts
@@ -8,41 +8,56 @@ type PlatformInfo = DeepReadonly<
   toStringObject(): Record<Keys<ExcludeKeysOfType<UAParser.IResult, AnyFunction>>, string>;
 };
 
-let result: PlatformInfo | undefined;
+let syncResult: PlatformInfo | undefined;
+let asyncResult: PlatformInfo | undefined;
 let promise: Promise<UAParser.IResult> | undefined;
 
+function toPlatformInfo(result: UAParser.IResult): PlatformInfo {
+  return {
+    // Is that order calls overrides values (withFeatureCheck().withClientHints()) ?
+    ...result,
+    toStringObject(this: PlatformInfo) {
+      return Object.getOwnPropertyNames(this).reduce(
+        (acc, key) => {
+          const prop = key as keyof typeof this;
+          if (this[prop] != null && typeof this[prop] !== 'function') {
+            acc[prop] = this[prop].toString();
+          }
+          return acc;
+        },
+        {} as Record<keyof PlatformInfo, string>
+      );
+    },
+  };
+}
+
+export function getPlatformInfoSync(): PlatformInfo {
+  if (syncResult == null) {
+    // Is that order calls overrides values (withFeatureCheck().withClientHints()) ?
+    syncResult = toPlatformInfo(new UAParser(navigator.userAgent).getResult().withFeatureCheck());
+  }
+  return syncResult;
+}
+
 export async function getPlatformInfo(): Promise<PlatformInfo> {
-  if (result == null) {
+  if (asyncResult == null) {
     promise =
       promise ??
       Promise.resolve(
         // Is that order calls overrides values (withFeatureCheck().withClientHints()) ?
         new UAParser(navigator.userAgent).getResult().withFeatureCheck().withClientHints()
       );
-    result = {
-      ...(await promise),
-      toStringObject(this: PlatformInfo) {
-        return Object.getOwnPropertyNames(this).reduce(
-          (acc, key) => {
-            const prop = key as keyof typeof this;
-            if (this[prop] != null && typeof this[prop] !== 'function') {
-              acc[prop] = this[prop].toString();
-            }
-            return acc;
-          },
-          {} as Record<keyof PlatformInfo, string>
-        );
-      },
-    };
+    asyncResult = toPlatformInfo(await promise);
+    syncResult = asyncResult;
     promise = undefined;
   }
-  return result;
+  return asyncResult;
 }
 
 export function getCachedPlatformInfo(): PlatformInfo | undefined {
-  if (result == null) {
+  if (asyncResult == null) {
     console.warn('PlatformInfo is not ready yet.');
     void getPlatformInfo();
   }
-  return result;
+  return asyncResult;
 }
