24e3e65a8a525020c15a388908a750889a8f4231
Vladimir <vzh.inbox@gmail.com>
2024-10-11 16:42:45 +0500

    Remove getAppContainer

--- src/getAppContainer.ts
+++
@@ -1,5 +0,0 @@
-export function getAppContainer(id = 'root'): HTMLElement {
-  const container = document.getElementById(id);
-  if (!container) throw new Error(`Container "${id}" for the app is not found.`);
-  return container;
-}
