e7fddc4e26130276de79e3edd999eaca9ff6a4f1
Vladimir <vzh.inbox@gmail.com>
2024-11-20 12:42:23 +0500

    Add support for sources

--- src/media/resetMedia.ts
+++ src/media/resetMedia.ts
@@ -1,11 +1,16 @@
-/* eslint-disable no-param-reassign */
-
-/** Remove media from hardware decoder buffer */
+/** Remove media from hardware decoder buffer. */
 export function resetMedia(media: HTMLMediaElement): void {
-  if (media.src || media.srcObject) {
+  if (media.src || media.srcObject || media.childElementCount > 0) {
     URL.revokeObjectURL(media.src);
     media.removeAttribute('src');
     media.srcObject = null;
+    if (media.childElementCount > 0) {
+      for (const element of media.children) {
+        if (element instanceof HTMLSourceElement) {
+          media.removeChild(element);
+        }
+      }
+    }
     media.load();
   }
 }
