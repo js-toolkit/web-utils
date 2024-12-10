dda732ff656232ea9142bae62944e05a8a2d2aa0
Vladimir <vzh.inbox@gmail.com>
2024-12-10 10:02:00 +0500

    Update jsdoc

--- src/getAspectRatio.ts
+++ src/getAspectRatio.ts
@@ -1,11 +1,11 @@
 import { farey } from '@js-toolkit/utils/farey';
 
 export interface AspectRatio {
-  /** Approximate width ratio */
+  /** Approximate ratio of the width. Eg. 16 of 16/9. */
   width: number;
-  /** Approximate height ratio */
+  /** Approximate ratio of the height. Eg. 9 of 16/9. */
   height: number;
-  /** Actual ratio value */
+  /** Actual ratio value. */
   ratio: number;
 }
 
