de466036dc89127436d312d7b0967fdb474e4152
Vladimir <vzh.inbox@gmail.com>
2025-05-27 12:17:31 +0500

    Improve toBase64

--- src/toBase64.ts
+++ src/toBase64.ts
@@ -1,3 +1,7 @@
+/** Safe for unicode string. */
 export function toBase64(str: string): string {
-  return window.btoa(unescape(encodeURIComponent(str)));
+  const binString = window.TextEncoder
+    ? Array.from(new TextEncoder().encode(str), (byte) => String.fromCodePoint(byte)).join('')
+    : unescape(encodeURIComponent(str));
+  return window.btoa(binString);
 }
